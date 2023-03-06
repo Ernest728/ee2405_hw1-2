27d3180f6bdd4537c82968b6e651ad6833154223
Ernest728 <41069663+Ernest728@users.noreply.github.com>
2023-03-06 10:33:48 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,178 @@
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+int main()
+{
+    DigitalOut myled(LED1);
+    
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(1s);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(1s);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(1s);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(1s);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(1s);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(1s);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+
+
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(1s);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(600ms);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(100ms);
+
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+        myled = 1;  // set LED1 pin to high
+        ThisThread::sleep_for(1s);
+        myled.write(0);  // set LED1 pin to low
+        ThisThread::sleep_for(200ms);
+}
+
